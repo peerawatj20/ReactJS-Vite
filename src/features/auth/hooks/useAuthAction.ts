87c98a2256fb0e82225581579659fa65fe3450f2
Peerawat J <peerawatjm@hotmail.com>
2025-09-25 17:28:29 +0700

    refactor: simplify handleLogin and handleLogout functions by removing useCallback

--- src/features/auth/hooks/useAuthAction.ts
+++ src/features/auth/hooks/useAuthAction.ts
@@ -1,5 +1,3 @@
-import { useCallback } from 'react';
-
 import { useAppDispatch } from '@/app/hooks';
 
 import type { LoginSchemaType } from '../schemas/login.schema';
@@ -8,16 +6,13 @@ import { login, logout } from '../state/auth.slice';
 export const useAuthAction = () => {
   const dispatch = useAppDispatch();
 
-  const handleLogin = useCallback(
-    async (credentials: LoginSchemaType) => {
-      await dispatch(login(credentials));
-    },
-    [dispatch],
-  );
+  const handleLogin = async (credentials: LoginSchemaType) => {
+    await dispatch(login(credentials));
+  };
 
-  const handleLogout = useCallback(() => {
+  const handleLogout = () => {
     dispatch(logout());
-  }, [dispatch]);
+  };
 
   return {
     handleLogin,
